9ca48d691c47942078fc78cb44b7ec5e7619c4b9
chaaanuwu <gkchanuwa@gmail.com>
2026-09-09 19:32:14 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# plotline_mobile
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/chaaanuwu/plotline_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.chaaanuwu.plotline_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
